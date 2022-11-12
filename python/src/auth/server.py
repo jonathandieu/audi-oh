ded1377fbd1503407b2ed65e61b1f75f6d7c30a0
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-11-11 22:39:47 -0800

    Get started on creating server

---
+++ python/src/auth/server.py
@@ -0,0 +1,9 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+# config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
