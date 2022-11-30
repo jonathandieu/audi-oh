36c4994c752ca4542af454d9e6682c4d595ca4e5
Jonathan Dieu <jonathanhuydieu@gmail.com>
2022-11-29 22:23:11 -0800

    Added environment config and create first login route

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -7,3 +7,13 @@ mysql = MySQL(server)
 
 # config
 server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PASSWORD_PORT")
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "missing credentials", 401
